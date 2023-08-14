d0e88527dd0fe894dd6ec83c3048db4d3d2ea4d3
BoPa <bopatynski@gmail.com>
2023-08-14 17:22:52 -0400

    Update main_index.js

--- main_index.js
+++ main_index.js
@@ -1,22 +1,16 @@
-const cfbtn = document.querySelector("[wb-data=cfbtn]");
-
-cfbtn.addEventListener("click", (e) => {
-  const btntxt = cfbtn.querySelector('[wb-data="txt"]');
-
-  btntxt.textContent = "Copying...";
-
-  const cpy = (e) => {
-    e.preventDefault();
-    const cjson = cfbtn.querySelector('[wb-data="j"]')
-      .textContent;
-    e.clipboardData.setData("application/json", cjson);
-  };
-
-  document.addEventListener("copy", cpy);
-  document.execCommand("copy");
-  document.removeEventListener("copy", cpy);
-
-  setTimeout(() => {
-    btntxt.textContent = "Copy to Destination";
-  }, 1000);
+const a = document.querySelector("[wb-data=copy-button]");
+a.addEventListener("click", (e) => {
+    const b = a.querySelector('[wb-data="text"]');
+    b.textContent = "Copying...";
+    const c = (e) => {
+        e.preventDefault();
+        const d = a.querySelector('[wb-data="json"]').textContent;
+        e.clipboardData.setData("application/json", d);
+    };
+    document.addEventListener("copy", c);
+    document.execCommand("copy");
+    document.removeEventListener("copy", c);
+    setTimeout(() => {
+        b.textContent = "Copy to Webflow";
+    }, 1000);
 });
